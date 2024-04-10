df58d9a0e705f2923104faf7c1d3d54c40c72333
charain <charain_li@outlook.com>
2024-04-10 17:35:52 +0800

    update

--- exercises/algorithm/algorithm3.rs
+++ exercises/algorithm/algorithm3.rs
@@ -1,13 +1,23 @@
 /*
-	sort
-	This problem requires you to implement a sorting algorithm
-	you can use bubble sorting, insertion sorting, heap sorting, etc.
+    sort
+    This problem requires you to implement a sorting algorithm
+    you can use bubble sorting, insertion sorting, heap sorting, etc.
 */
-// I AM NOT DONE
 
-fn sort<T>(array: &mut [T]){
-	//TODO
+use std::mem::swap;
+
+fn sort<T: std::cmp::PartialOrd + Clone>(mut array: &mut [T]) {
+    for i in 0..array.len() {
+        for j in (i + 1)..array.len() {
+            if array[i] > array[j] {
+                let tmp = array[i].clone();
+                array[i] = array[j].clone();
+                array[j] = tmp;
+            }
+        }
+    }
 }
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -18,16 +28,17 @@ mod tests {
         sort(&mut vec);
         assert_eq!(vec, vec![19, 37, 46, 57, 64, 73, 75, 91]);
     }
-	#[test]
+    #[test]
     fn test_sort_2() {
         let mut vec = vec![1];
         sort(&mut vec);
         assert_eq!(vec, vec![1]);
     }
-	#[test]
+    #[test]
     fn test_sort_3() {
         let mut vec = vec![99, 88, 77, 66, 55, 44, 33, 22, 11];
         sort(&mut vec);
         assert_eq!(vec, vec![11, 22, 33, 44, 55, 66, 77, 88, 99]);
     }
 }
+
